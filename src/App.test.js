58232fc0fb8bd0a6fdef8b2123bf20e65c80a55f
jcorrea <juan.correa@wizeline.com>
2021-07-21 13:26:01 -0500

    added eslint and prettier config, replace content to hello world

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders hello world', () => {
+  render(<App />)
+  const helloElement = screen.getByText(/hello react bootcamp - wizeline :D/i)
+  expect(helloElement).toBeInTheDocument()
+})
